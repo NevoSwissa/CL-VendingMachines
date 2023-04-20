754888ceefedee092035deda99bf0f67b8afb18a
NevoSwissa <96447671+NevoSwissa@users.noreply.github.com>
2023-04-20 23:51:34 +0300

    Update CL-SellMachines.sql

--- CL-SellMachines/CL-SellMachines.sql
+++ CL-SellMachines/CL-SellMachines.sql
@@ -1,19 +1,3 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               10.4.27-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win64
--- HeidiSQL Version:             11.3.0.6295
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
--- Dumping structure for table qbcoreframework_fc0597.cl_sellmachines
 CREATE TABLE IF NOT EXISTS `cl_sellmachines` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `citizenid` varchar(50) DEFAULT NULL,
@@ -24,11 +8,3 @@ CREATE TABLE IF NOT EXISTS `cl_sellmachines` (
   PRIMARY KEY (`id`),
   KEY `citizenid` (`citizenid`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-/*!40000 ALTER TABLE `cl_sellmachines` DISABLE KEYS */;
-/*!40000 ALTER TABLE `cl_sellmachines` ENABLE KEYS */;
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
